cd3df61ccdbdd1a1659e40d094d095a15f336a29
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-08 16:34:18 -0500

    ForgotPassword

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
     <>
       <SignUp />
       <SignIn />
+      <ForgotPassword />
     </>
   );
 }
