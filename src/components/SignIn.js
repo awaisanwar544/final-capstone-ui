d30375fc0a6a7dedb1ea1ad0a4e32f9df170fc06
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-08 16:28:15 -0500

    Small visual addition

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,13 +5,13 @@ function SignIn() {
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="email">
             E-mail
-            <input type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 " id="email" placeholder="Email@something.com" />
+            <input type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="email" placeholder="Email@something.com" />
           </label>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
             Password
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500" id="password" type="password" placeholder="******************" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="password" type="password" placeholder="******************" />
           </label>
         </div>
         <div className="flex items-center justify-between">
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,19 +5,19 @@ function SignUp() {
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
             Username
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 " id="username" type="text" placeholder="Username" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="username" type="text" placeholder="Username" />
           </label>
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="email">
             E-mail
-            <input type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 " id="email" placeholder="Email@something.com" />
+            <input type="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="email" placeholder="Email@something.com" />
           </label>
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
             Password
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500" id="password" type="password" placeholder="******************" />
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="password" type="password" placeholder="******************" />
           </label>
         </div>
         <div className="flex items-center justify-center">
