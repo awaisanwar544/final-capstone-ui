b8a24b73efc637183897e1be45daf8525c5dbc09
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-08 18:51:05 -0500

    Moving user components to pages folder

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import SplashPage from './pages/SplashPage';
 
-// import SignUp from './components/SignUp';
-// import SignIn from './components/SignIn';
-// import ForgotPassword from './components/ForgotPassword';
+// import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
+// import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   const [splash, setSplash] = useState(true);
