c44038f1b7bdf61cb511df75873733283832b909
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-08 23:39:45 -0500

    Adding icons

--- src/components/ProviderCard.js
+++ src/components/ProviderCard.js
@@ -1,6 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter, faGithub, faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+
 function ProviderCard() {
   return (
-    <div className="flex flex-col w-3/4 mx-6">
+    <div className="flex flex-col mx-6">
       <div className="item w-auto h-40">
         <img className="w-40 h-40 m-auto object-contain rounded-full bg-custom-green-500" src="https://w7.pngwing.com/pngs/905/43/png-transparent-management-professional-sales-business-service-lawyer-company-service-people-thumbnail.png" alt="Person" />
       </div>
@@ -10,10 +15,22 @@ function ProviderCard() {
         <p className="text-custom-grey-500">People that something</p>
       </div>
       <div className="item w-auto h-12">
-        <ul className="flex justify-center">
-          <li><a href="https://github.com/Kakalanp">Github</a></li>
-          <li><a href="https://www.linkedin.com/in/joaquin-garrido-lecca-zanetti/">LinkedIn</a></li>
-          <li><a href="https://twitter.com/LeccaJoaquin">Twitter</a></li>
+        <ul className="flex w-52 justify-around">
+          <li>
+            <a className="rounded-full border-2 border-custom-gray-500 p-1" href="https://github.com/Kakalanp" without rel="noreferrer" target="_blank">
+              <FontAwesomeIcon className="w-5 text-custom-grey-500 bg:white-500 text-xl" icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a className="rounded-full border-2 border-custom-gray-500 p-1" href="https://www.linkedin.com/in/joaquin-garrido-lecca-zanetti/" without rel="noreferrer" target="_blank">
+              <FontAwesomeIcon className="w-5 text-custom-grey-500 bg:white-500 text-xl" icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a className="rounded-full border-2 border-custom-gray-500 p-1" href="https://twitter.com/LeccaJoaquin" without rel="noreferrer" target="_blank">
+              <FontAwesomeIcon className="w-5 text-custom-grey-500 bg:white-500 text-xl" icon={faTwitter} />
+            </a>
+          </li>
         </ul>
       </div>
     </div>
