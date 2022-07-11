7a745a2dfaa466850e8e8e00c1049c773cf48d1c
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-07-11 10:41:04 -0500

    Updating width

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -2,7 +2,7 @@ import Reservation from '../components/ReservationCard';
 
 function MyReservations() {
   return (
-    <div className="flex flex-col items-center h-screen overflow-y-scroll">
+    <div className="flex flex-col items-center h-screen w-full overflow-y-scroll">
       <h1 className="text-3xl my-4 font-bold">My reservations:</h1>
       <Reservation />
       <Reservation />
