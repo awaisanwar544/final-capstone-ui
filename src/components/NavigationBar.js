fd119110cad2c7ec4f9fca4aa9e97515fb9ca40c
awaisanwar544 <awaisanwar544@gmail.com>
2022-07-14 08:11:44 +0500

    Add logout button

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,14 +6,14 @@ import {
 import logo from '../assets/logo.png';
 
 function NavigationBar() {
-  const inactiveClassName = 'flex flex-col bg-custom-white-500 text-custom-grey-500 text-xl font-extrabold h-20 justify-center p-5 hover:text-custom-white-500 hover:bg-custom-green-500';
-  const activeClassName = 'flex flex-col bg-custom-green-500 text-custom-white-500 text-xl font-extrabold h-20 justify-center p-5';
+  const inactiveClassName = 'flex flex-col bg-custom-white-500 text-custom-grey-500 text-xl font-bold justify-center h-16 p-5 hover:text-custom-white-500 hover:bg-custom-green-500';
+  const activeClassName = 'flex flex-col bg-custom-green-500 text-custom-white-500 text-xl font-bold h-16 justify-center p-5';
   return (
-    <div className="flex flex-col justify-between p-5 pr-0 items-center bg-custom-white-500 h-screen w-1/5 shadow-xl">
+    <div className="flex flex-col p-5 pr-0 items-center bg-custom-white-500 h-screen w-1/5 shadow-xl">
       <a href="/" className="w-24">
         <img className="object-contain" src={logo} alt="logo" />
       </a>
-      <div className="w-full pl-10 space-y-2">
+      <div className="w-full pl-10 mt-20 space-y-1">
         <NavLink
           exact
           to="/"
@@ -43,7 +43,12 @@ function NavigationBar() {
           <p>Delete Developer</p>
         </NavLink>
       </div>
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex items-center justify-center mt-20">
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white font-bold py-2 px-4 rounded focus:outline-none" type="button">
+          Logout
+        </button>
+      </div>
+      <div className="flex flex-col items-center gap-4 mt-auto">
         <div className="flex gap-x-6">
           <a href="https://www.google.com/">
             <FontAwesomeIcon className="text-custom-grey-500 text-xl hover:text-custom-green-500" icon={faTwitter} />
