007e7e9ea9ee60410c9a457f9363d1f599f66aac
awaisanwar544 <awaisanwar544@gmail.com>
2022-07-18 22:29:28 +0500

    Add date validations on new reservation page

--- src/components/NewReservationForm.js
+++ src/components/NewReservationForm.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { DateTime } from 'luxon';
 
 import { newReservations } from '../redux/reducers/reservations';
 import { getProviders } from '../redux/reducers/providers';
@@ -14,25 +15,61 @@ function NewReservationForm() {
   const navigate = useNavigate();
 
   const [totalCost, setTotalCost] = useState(0);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  const [startDate, setStartDate] = useState(false);
+  const [endDate, setEndDate] = useState(false);
+  const [message, setMessage] = useState(false);
 
   const changeStartDate = (event) => {
     event.preventDefault();
-    const date = new Date(event.target.value);
-    setStartDate(date);
+    const today = DateTime.now();
+    const date = DateTime.fromISO(event.target.value);
+    if (date.ordinal <= today.ordinal) {
+      setMessage(`Start date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+    }
+
+    if (endDate.ordinal < date.ordinal) {
+      setMessage('End Date must be after Start Date');
+    }
+
+    if (date.ordinal > today.ordinal && endDate.ordinal > date.ordinal) {
+      setMessage(false);
+      setStartDate(date);
+    }
+
+    if (!endDate && date.ordinal > today.ordinal) {
+      setMessage(false);
+      setStartDate(date);
+    }
   };
 
   const changeEndDate = (event) => {
     event.preventDefault();
-    const date = new Date(event.target.value);
-    setEndDate(date);
+    const today = DateTime.now();
+    const date = DateTime.fromISO(event.target.value);
+
+    if (date.ordinal <= today.ordinal) {
+      setMessage(`Start date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+    }
+
+    if (startDate && startDate.ordinal > date.ordinal) {
+      setMessage('End Date must be after Start Date');
+    }
+
+    if (!startDate && date.ordinal > today.ordinal) {
+      setMessage('Please Select the start date also.');
+      setEndDate(date);
+    }
+
+    if (date.ordinal > today.ordinal && date.ordinal > startDate.ordinal) {
+      setMessage(false);
+      setEndDate(date);
+    }
   };
 
   const calculateTotalCost = () => {
     const difference = endDate - startDate;
     const numberOfDays = Math.ceil(difference / (1000 * 3600 * 24));
-    if (numberOfDays > 0) {
+    if (startDate && endDate && numberOfDays > 0) {
       return numberOfDays * Math.floor(provider.cost);
     }
 
@@ -63,6 +100,8 @@ function NewReservationForm() {
         ? (
           <div className="flex flex-col w-full h-screen flex items-center justify-center space-y-10">
             <h1 className="text-4xl text-custom-grey-500">{`To hire ${provider.name} fill in the following details`}</h1>
+            {message
+              ? <p className="text-red-500">{message}</p> : ''}
             <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 h-fit mx-auto" onSubmit={handleSubmit}>
               <div className="flex flex-col space-y-6">
                 <div className="mb-4">
@@ -78,10 +117,10 @@ function NewReservationForm() {
                   </label>
                 </div>
                 <div className="mb-4">
-                  <div className="block text-gray-700 text-lg font-bold mb-2" htmlFor="profile-image">
+                  <div className="flex justify-between text-gray-700 text-lg font-bold mb-2" htmlFor="profile-image">
                     Total Cost:
                     <span className="text-custom-green-500">
-                      { totalCost ? ` ${totalCost} $` : ' Invalid Dates' }
+                      { totalCost ? `$ ${totalCost.toFixed(2)}` : ' Invalid Dates' }
                     </span>
                   </div>
                 </div>
