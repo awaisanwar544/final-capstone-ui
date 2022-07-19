33d1569951725c0966145ebb989d39504e021577
awaisanwar544 <awaisanwar544@gmail.com>
2022-07-20 04:56:44 +0500

    fixed menu button click issue & update validations

--- src/components/NewProviderForm.js
+++ src/components/NewProviderForm.js
@@ -56,10 +56,7 @@ function NewProviderForm() {
     dispatch(getSkills());
   }, []);
   return (
-    <motion.div
-      animate={{ x: 0, opacity: 1, rotate: 0 }}
-      initial={{ x: '100%', opacity: 0, rotate: 20 }}
-      transition={{ duration: 0.5 }}
+    <div
       className="flex flex-col w-full h-screen flex items-center justify-center space-y-10"
     >
       <h1 className="text-4xl text-custom-grey-500 mt-40 md:mt-0">Add New Developer</h1>
@@ -68,96 +65,102 @@ function NewProviderForm() {
         <span className="text-red-500">*</span>
         ) are required
       </p>
-      <form className="bg-white mx-auto" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row md:space-x-10">
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-              Full Name
-              <span className="text-red-500"> *</span>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="name" type="text" placeholder="Full Name" required />
-            </label>
+      <motion.div
+        animate={{ x: 0, opacity: 1, rotate: 0 }}
+        initial={{ x: '100%', opacity: 0, rotate: 20 }}
+        transition={{ duration: 0.5 }}
+      >
+        <form className="bg-white mx-auto" onSubmit={handleSubmit}>
+          <div className="flex flex-col md:flex-row md:space-x-10">
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                Full Name
+                <span className="text-red-500"> *</span>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="name" type="text" placeholder="Full Name" required />
+              </label>
+            </div>
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rate">
+                Rate
+                <span className="text-red-500"> *</span>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="rate" type="number" placeholder="Rate per Day" required />
+              </label>
+            </div>
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="profile-image">
+                Profile Image
+                <span className="text-red-500"> *</span>
+                <input className="form-control block w-full text-base font-normal file:py-1.5 file:px-3 text-custom-grey-500 bg-custom-white-500 bg-clip-padding border border-solid border-custom-green-500 rounded transition ease-in-out m-0 focus:text-custom-green-500 focus:bg-custom-white-500 focus:border-custom-green-500 focus:outline-none file:bg-custom-green-500 file:rounded file:border-none file:text-custom-white-500 file:cursor-pointer" id="profile-image" type="file" placeholder="Upload Profile Image" accept="image/*" required />
+              </label>
+            </div>
           </div>
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rate">
-              Rate
-              <span className="text-red-500"> *</span>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="rate" type="number" placeholder="Rate per Day" required />
-            </label>
+          <div className="flex flex-col md:flex-row md:space-x-10 justify-between md:justify-start">
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="bio">
+                Bio
+                <span className="text-red-500"> *</span>
+                <textarea type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" rows="5" cols="50" id="bio" placeholder="Bio (Max 100 characters)" maxLength="100" required />
+              </label>
+            </div>
+            <div className="md:w-2/5 w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-4 max-h-36 overflow-y-scroll" htmlFor="skills">
+                Skills
+                <span className="text-red-500"> *</span>
+                { skillsError
+                  ? <p className="text-red-500">{skillsError}</p>
+                  : '' }
+                <br />
+                {skills.map((skill) => (
+                  <div key={skill.id}>
+                    <input type="checkbox" className="checked:accent-custom-green-500" id={`skill-${skill.id}`} name="skills" value={skill.name} />
+                    {` ${skill.name}`}
+                    <br />
+                  </div>
+                ))}
+              </label>
+            </div>
           </div>
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="profile-image">
-              Profile Image
-              <span className="text-red-500"> *</span>
-              <input className="form-control block w-full text-base font-normal file:py-1.5 file:px-3 text-custom-grey-500 bg-custom-white-500 bg-clip-padding border border-solid border-custom-green-500 rounded transition ease-in-out m-0 focus:text-custom-green-500 focus:bg-custom-white-500 focus:border-custom-green-500 focus:outline-none file:bg-custom-green-500 file:rounded file:border-none file:text-custom-white-500 file:cursor-pointer" id="profile-image" type="file" placeholder="Upload Profile Image" accept="image/*" required />
-            </label>
+          <div>
+            <p className=" text-gray-700 text-sm font-bold">Add links to the social profiles:</p>
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="github">
+                <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
+                  Github
+                  {' '}
+                  <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faGithub} />
+                </p>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="github" type="text" placeholder="Github profile link" />
+              </label>
+            </div>
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="linkedin">
+                <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
+                  LinkedIn
+                  {' '}
+                  <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faLinkedin} />
+                </p>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="linkedin" type="text" placeholder="Linkedin profile link" />
+              </label>
+            </div>
+            <div className="">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="twitter">
+                <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
+                  Twitter
+                  {' '}
+                  <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faTwitter} />
+                </p>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="twitter" type="text" placeholder="Twitter profile link" />
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col md:flex-row md:space-x-10 justify-between md:justify-start">
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-4" htmlFor="bio">
-              Bio
-              <span className="text-red-500"> *</span>
-              <textarea type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" rows="5" cols="50" id="bio" placeholder="Bio (Max 100 characters)" maxLength="100" required />
-            </label>
+          <div className="flex items-center justify-center">
+            <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white font-bold py-2 px-4 rounded focus:outline-none" type="submit">
+              Add Developer
+            </button>
           </div>
-          <div className="md:w-2/5 w-full">
-            <label className="block text-gray-700 text-sm font-bold mb-4 max-h-36 overflow-y-scroll" htmlFor="skills">
-              Skills
-              <span className="text-red-500"> *</span>
-              { skillsError
-                ? <p className="text-red-500">{skillsError}</p>
-                : '' }
-              <br />
-              {skills.map((skill) => (
-                <div key={skill.id}>
-                  <input type="checkbox" className="checked:accent-custom-green-500" id={`skill-${skill.id}`} name="skills" value={skill.name} />
-                  {` ${skill.name}`}
-                  <br />
-                </div>
-              ))}
-            </label>
-          </div>
-        </div>
-        <div>
-          <p className=" text-gray-700 text-sm font-bold">Add links to the social profiles:</p>
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="github">
-              <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
-                Github
-                {' '}
-                <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faGithub} />
-              </p>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="github" type="text" placeholder="Github profile link" />
-            </label>
-          </div>
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="linkedin">
-              <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
-                LinkedIn
-                {' '}
-                <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faLinkedin} />
-              </p>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="linkedin" type="text" placeholder="Linkedin profile link" />
-            </label>
-          </div>
-          <div className="">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="twitter">
-              <p className="w-fit mb-1 rounded-full border-2 border-custom-gray-500 hover:border-custom-green-500 text-custom-grey-500 hover:text-custom-green-500 p-1" without="true" rel="noreferrer" target="_blank">
-                Twitter
-                {' '}
-                <FontAwesomeIcon className="w-5 bg:white-500 text-xl" icon={faTwitter} />
-              </p>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="twitter" type="text" placeholder="Twitter profile link" />
-            </label>
-          </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white font-bold py-2 px-4 rounded focus:outline-none" type="submit">
-            Add Developer
-          </button>
-        </div>
-      </form>
-    </motion.div>
+        </form>
+      </motion.div>
+    </div>
   );
 }
 
--- src/components/NewReservationForm.js
+++ src/components/NewReservationForm.js
@@ -27,34 +27,41 @@ function NewReservationForm() {
     const difference = date.diff(today, ['months', 'days']);
     const diffInDays = Math.ceil(date.diff(today, 'days').days);
 
-    if (difference.months >= 6 && difference.days > 0) {
-      setMessage(`You can only book a developer 6 or less months advance. Your current selection is ${difference.months} and ${Math.ceil(difference.days)} days away`);
-    }
-
-    if (difference.months < 6) {
-      if (diffInDays < 1) {
-        setMessage(`Start date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+    if (!endDate) {
+      if (difference.months >= 6 && difference.days > 0) {
+        setMessage(`You can only book a developer 6 or less months advance. Your current selection is ${difference.months} and ${Math.ceil(difference.days)} days away`);
+        setStartDate(false);
       }
 
-      if (diffInDays > 0) {
-        setMessage(false);
-        setStartDate(date);
+      if (difference.months < 6) {
+        if (diffInDays < 1) {
+          setMessage(`Start date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+          setStartDate(false);
+        }
+
+        if (diffInDays > 0) {
+          setMessage('Select the end date');
+          setStartDate(date);
+        }
       }
     }
 
     if (endDate) {
       const duration = Math.ceil(endDate.diff(date, 'days').days);
 
-      if (duration < 1) {
-        setMessage('End date must be later than the start date');
+      if (duration < 0) {
+        setMessage('End date must be equal or later than the start date');
+        setStartDate(false);
       }
 
       if (duration > 30) {
         setMessage(`You can book a developer for 30 days. Currently your duration is ${duration} days.`);
+        setStartDate(false);
       }
 
       if (diffInDays < 1) {
         setMessage(`Start date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+        setStartDate(false);
       }
 
       if (duration <= 30 && duration > 0 && diffInDays > 0) {
@@ -72,21 +79,24 @@ function NewReservationForm() {
 
     if (diffInDays < 1) {
       setMessage(`End date must be later than today. ${DateTime.now().toLocaleString(DateTime.DATE_MED)} `);
+      setEndDate(false);
     }
 
     if (diffInDays > 0) {
       if (startDate) {
         const duration = Math.ceil(date.diff(startDate, 'days').days);
 
-        if (duration < 1) {
-          setMessage('End date must be later than the start date');
+        if (duration < 0) {
+          setMessage('End date must be equal or later than the start date');
+          setEndDate(false);
         }
 
         if (duration > 30) {
           setMessage(`You can book a developer for 30 days. Currently your duration is ${duration} days.`);
+          setEndDate(false);
         }
 
-        if (duration <= 30 && duration > 0) {
+        if (duration <= 30 && duration >= 0) {
           setMessage(false);
           setEndDate(date);
         }
@@ -101,7 +111,7 @@ function NewReservationForm() {
 
   const calculateTotalCost = () => {
     if (startDate && endDate) {
-      const numberOfDays = Math.ceil(endDate.diff(startDate, 'days').days);
+      const numberOfDays = Math.ceil(endDate.diff(startDate, 'days').days) + 1;
       return numberOfDays * Math.floor(provider.cost);
     }
 
@@ -131,45 +141,48 @@ function NewReservationForm() {
     <>
       {provider
         ? (
-          <motion.div
-            animate={{ x: 0, opacity: 1, rotate: 0 }}
-            initial={{ x: '100%', opacity: 0, rotate: 20 }}
-            transition={{ duration: 0.5 }}
+          <div
             className="flex flex-col w-full h-screen flex items-center justify-center space-y-10"
           >
             <h1 className="text-xl px-5 md:text-4xl text-custom-grey-500">{`To hire ${provider.name} fill in the following details`}</h1>
             {message
               ? <p className="text-red-500">{message}</p> : ''}
-            <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 h-fit mx-auto" onSubmit={handleSubmit}>
-              <div className="flex flex-col space-y-6">
-                <div className="mb-4">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                    Start Date
-                    <input onChange={changeStartDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="date" type="date" required />
-                  </label>
-                </div>
-                <div className="mb-6">
-                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rate">
-                    End Date
-                    <input onChange={changeEndDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="rate" type="date" required />
-                  </label>
-                </div>
-                <div className="mb-4">
-                  <div className="flex justify-between text-gray-700 text-lg font-bold mb-2" htmlFor="profile-image">
-                    Total Cost:
-                    <span className="text-custom-green-500">
-                      { totalCost ? `$ ${totalCost.toFixed(2)}` : ' Invalid Dates' }
-                    </span>
+            <motion.div
+              animate={{ x: 0, opacity: 1, rotate: 0 }}
+              initial={{ x: '100%', opacity: 0, rotate: 20 }}
+              transition={{ duration: 0.5 }}
+            >
+              <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 h-fit mx-auto" onSubmit={handleSubmit}>
+                <div className="flex flex-col space-y-6">
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                      Start Date
+                      <input onChange={changeStartDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="date" type="date" required />
+                    </label>
+                  </div>
+                  <div className="mb-6">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="rate">
+                      End Date
+                      <input onChange={changeEndDate} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-custom-green-500 focus:placeholder:text-custom-green-500" id="rate" type="date" required />
+                    </label>
+                  </div>
+                  <div className="mb-4">
+                    <div className="flex justify-between text-gray-700 text-lg font-bold mb-2" htmlFor="profile-image">
+                      Total Cost:
+                      <span className="text-custom-green-500">
+                        { totalCost ? `$ ${totalCost.toFixed(2)}` : ' Invalid Dates' }
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex items-center justify-center">
-                <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white font-bold py-2 px-4 mt-6 rounded focus:outline-none" type="submit">
-                  Hire Developer
-                </button>
-              </div>
-            </form>
-          </motion.div>
+                <div className="flex items-center justify-center">
+                  <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white font-bold py-2 px-4 mt-6 rounded focus:outline-none" type="submit">
+                    Hire Developer
+                  </button>
+                </div>
+              </form>
+            </motion.div>
+          </div>
         ) : 'Wait a moment while we fetch data'}
     </>
   );
