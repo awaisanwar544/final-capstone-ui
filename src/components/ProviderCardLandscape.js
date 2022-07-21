f69ba31dc9428a40c9f53de0dd97a28080c3d6d8
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-07-21 01:05:30 -0300

    Add confirmation delete buttons

--- src/components/ProviderCardLandscape.js
+++ src/components/ProviderCardLandscape.js
@@ -6,28 +6,34 @@ import { delProviders } from '../redux/reducers/providers';
 
 function ProviderCard({ providerData }) {
   const dispatch = useDispatch();
-  let clicks = 0;
 
   const deleteConfirmation = () => {
     const card = document.getElementById(`provider-${providerData.id}`);
-    card.classList.add('text-custom-grey-500', 'hover:border-red-500');
-    card.childNodes[0].classList.add('grayscale');
-    card.childNodes[1].innerHTML = `Are you sure you want to delete ${providerData.name}?`;
-    const button = card.childNodes[2].childNodes[0];
-    button.classList.add('border-red-500');
-    button.innerHTML = 'Yes';
-  };
+    card.classList.add('hover:border-red-500');
+    card.childNodes[0].childNodes[0].classList.add('grayscale');
+    card.childNodes[0].childNodes[1].childNodes[1].classList.add('hidden');
+    card.childNodes[0].childNodes[1].childNodes[0].innerHTML = `Are you sure you want to delete ${providerData.name}?`;
+    const button = card.childNodes[1].childNodes[0];
+    const yesButton = card.childNodes[1].childNodes[1];
+    const noButton = card.childNodes[1].childNodes[2];
+    button.classList.add('hidden');
+    yesButton.classList.remove('hidden');
+    noButton.classList.remove('hidden');
 
-  const deleteProvider = () => {
-    clicks += 1;
-    switch (clicks) {
-      default:
-        deleteConfirmation();
-        break;
-      case 2:
-        dispatch(delProviders(providerData.id));
-        document.getElementById(`provider-${providerData.id}`).remove();
-    }
+    yesButton.addEventListener('click', () => {
+      dispatch(delProviders(providerData.id));
+      card.remove();
+    });
+
+    noButton.addEventListener('click', () => {
+      card.classList.remove('text-custom-grey-500', 'hover:border-red-500');
+      card.childNodes[0].childNodes[0].classList.remove('grayscale');
+      card.childNodes[0].childNodes[1].childNodes[0].innerHTML = `${providerData.name}`;
+      card.childNodes[0].childNodes[1].childNodes[1].classList.remove('hidden');
+      noButton.classList.add('hidden');
+      yesButton.classList.add('hidden');
+      button.classList.remove('hidden');
+    });
   };
 
   return (
@@ -37,19 +43,26 @@ function ProviderCard({ providerData }) {
       initial={{ x: '100%', opacity: 0, rotate: 20 }}
       transition={{ duration: 0.5 }}
       id={`provider-${providerData.id}`}
-      className="flex justify-around items-center mt-8 py-4 shadow-lg w-3/4 border-2 border-transparent hover:border-custom-green-500 rounded-md hover:shadow-2xl transition hover:scale-110 duration:300ms"
+      className="flex flex-col md:flex-row justify-around items-center mt-8 py-4 shadow-lg w-3/4 border-2 border-transparent hover:border-custom-green-500 rounded-md hover:shadow-2xl transition hover:scale-110 duration:300ms"
     >
-      <div className="item w-auto h-20">
-        <img className="w-20 h-20 m-auto object-cover rounded-full bg-custom-green-500" src={providerData.image} alt={providerData.name} />
+      <div className="flex grow justify-around items-center">
+        <div className="item w-auto h-20">
+          <img className="w-20 h-20 m-auto object-cover rounded-full bg-custom-green-500" src={providerData.image} alt={providerData.name} />
+        </div>
+        <div className="item w-4/5 h-28 text-center flex flex-wrap flex-col justify-around overflow-hidden">
+          <h3 className="font-bold">{providerData.name}</h3>
+          <p className="text-custom-grey-500">{providerData.bio}</p>
+        </div>
       </div>
-      <div className="item w-3/5 h-28 text-center flex flex-wrap flex-col justify-around overflow-hidden">
-        <h3>{providerData.name}</h3>
-        <span className="text-custom-grey-500 select-none">..................</span>
-        <p className="text-custom-grey-500">{providerData.bio}</p>
-      </div>
-      <div className="flex items-center justify-center">
-        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={deleteProvider}>
+      <div className="flex items-center justify-center md:mr-4">
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={deleteConfirmation}>
           Delete
+        </button>
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-red-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none ml-2 hidden" type="button">
+          Yes
+        </button>
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white hover:border-custom-green-500 font-bold py-2 px-4 rounded focus:outline-none ml-2 hidden" type="button">
+          No
         </button>
       </div>
     </motion.div>
--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -15,6 +15,36 @@ function Reservation({ reservationData }) {
     deleteElement();
   };
 
+  const deleteConfirmation = () => {
+    const card = document.getElementById(`reservation-${reservationData.id}`);
+    card.classList.add('hover:border-red-500');
+    card.childNodes[0].childNodes[0].classList.add('grayscale');
+    card.childNodes[1].childNodes[0].childNodes[0].innerHTML = `Are you sure you want to delete ${reservationData.provider_name}?`;
+    card.childNodes[1].childNodes[0].childNodes[1].classList.add('hidden');
+    card.childNodes[1].childNodes[0].childNodes[2].classList.add('hidden');
+    card.childNodes[1].childNodes[0].childNodes[3].classList.add('hidden');
+    const button = card.childNodes[2].childNodes[0];
+    const yesButton = card.childNodes[2].childNodes[1];
+    const noButton = card.childNodes[2].childNodes[2];
+    button.classList.add('hidden');
+    yesButton.classList.remove('hidden');
+    noButton.classList.remove('hidden');
+    noButton.addEventListener('click', () => {
+      card.classList.remove('hover:border-red-500');
+      card.childNodes[0].childNodes[0].classList.remove('grayscale');
+      card.childNodes[1].childNodes[0].childNodes[0].innerHTML = `You reserved: ${reservationData.provider_name}`;
+      card.childNodes[1].childNodes[0].childNodes[1].classList.remove('hidden');
+      card.childNodes[1].childNodes[0].childNodes[2].classList.remove('hidden');
+      card.childNodes[1].childNodes[0].childNodes[3].classList.remove('hidden');
+      noButton.classList.add('hidden');
+      yesButton.classList.add('hidden');
+      button.classList.remove('hidden');
+    });
+    yesButton.addEventListener('click', () => {
+      deleteReservation();
+    });
+  };
+
   return (
     <motion.div
       whileHover={{ scale: 1.1 }}
@@ -45,8 +75,14 @@ function Reservation({ reservationData }) {
         </div>
       </div>
       <div className="flex items-center justify-center">
-        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={deleteReservation}>
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none" type="button" onClick={deleteConfirmation}>
           Delete
+        </button>
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-red-500 hover:bg-red-500 hover:text-white hover:border-red-500 font-bold py-2 px-4 rounded focus:outline-none ml-2 hidden" type="button">
+          Yes
+        </button>
+        <button className="bg-custom-white-500 text-custom-grey-500 border border-custom-green-500 hover:bg-custom-green-500 hover:text-white hover:border-custom-green-500 font-bold py-2 px-4 rounded focus:outline-none ml-2 hidden" type="button">
+          No
         </button>
       </div>
     </motion.div>
